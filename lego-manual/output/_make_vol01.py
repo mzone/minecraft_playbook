7f1ce27c60c9a5d17b68038f57e385a5f6e013ad
Imanishi.Masakazu <macfoce@gmail.com>
2026-06-11 13:37:28 +0900

    no message

--- lego-manual/output/_make_vol01.py
+++ lego-manual/output/_make_vol01.py
@@ -4,8 +4,8 @@ from reportlab.lib.pagesizes import A4, landscape
 from reportlab.lib.utils import ImageReader
 from PIL import Image as PILImage
 
-paths = ["/Users/imamuu/Documents/project/minecraft_playbook/data/lego-manual/output/vol01_step_01.png","/Users/imamuu/Documents/project/minecraft_playbook/data/lego-manual/output/vol01_step_02.png","/Users/imamuu/Documents/project/minecraft_playbook/data/lego-manual/output/vol01_step_03.png","/Users/imamuu/Documents/project/minecraft_playbook/data/lego-manual/output/vol01_step_04.png"]
-out   = "/Users/imamuu/Documents/project/minecraft_playbook/data/lego-manual/output/vol01_ひみつきち.pdf"
+paths = ["/Users/imamuu/Documents/project/minecraft_playbook/data/lego-manual/output/vol01_step_01.png","/Users/imamuu/Documents/project/minecraft_playbook/data/lego-manual/output/vol01_step_02.png","/Users/imamuu/Documents/project/minecraft_playbook/data/lego-manual/output/vol01_step_03.png","/Users/imamuu/Documents/project/minecraft_playbook/data/lego-manual/output/vol01_step_04.png","/Users/imamuu/Documents/project/minecraft_playbook/data/lego-manual/output/vol01_step_05.png","/Users/imamuu/Documents/project/minecraft_playbook/data/lego-manual/output/vol01_step_06.png","/Users/imamuu/Documents/project/minecraft_playbook/data/lego-manual/output/vol01_step_07.png","/Users/imamuu/Documents/project/minecraft_playbook/data/lego-manual/output/vol01_step_08.png","/Users/imamuu/Documents/project/minecraft_playbook/data/lego-manual/output/vol01_step_09.png","/Users/imamuu/Documents/project/minecraft_playbook/data/lego-manual/output/vol01_step_10.png","/Users/imamuu/Documents/project/minecraft_playbook/data/lego-manual/output/vol01_step_11.png","/Users/imamuu/Documents/project/minecraft_playbook/data/lego-manual/output/vol01_step_12.png"]
+out   = "/Users/imamuu/Documents/project/minecraft_playbook/data/lego-manual/output/vol01_きのいえ.pdf"
 
 page_w, page_h = landscape(A4)
 margin = 12
